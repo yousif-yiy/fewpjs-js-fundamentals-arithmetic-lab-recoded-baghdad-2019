1536d166b50ce2963c28f48a404fec0ca9572c1d
yousif diyhaa <yousif7733@gmail.com>
2019-11-24 09:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const var1 = 2
 const var2 = 31
+<<<<<<< HEAD
 const multiply = var1 * var2
+=======
+const multiply = 62
+>>>>>>> 4001a35e94b9c7323e6a0e85a1071f3e72e769e4
 const random = Math.floor( Math.random() * 1000 ) + 1
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
